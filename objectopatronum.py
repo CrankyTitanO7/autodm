b4a79140801ac412cd07d2d9d51ada7a23ae9e02
CrankyTitanO7 <nedajtheawesome@gmail.com>
2024-07-16 06:33:07 -0400

    created objectopatronum not working createmonster() func

---
+++ objectopatronum.py
@@ -0,0 +1,25 @@
+import random
+
+class speels:
+    ice_learned = False
+    fire_learned = False
+    lightning_learned = False
+
+class monsters:
+    monnies = {
+        "vilespawn" : "alive",
+        "ogre" : "alive",
+        "minotaur" : "alive"
+    }
+
+    def createMonster(name):
+        name = input()
+        monsters.monnies[name] = "alive"
+    
+    def listmons():
+        print(monsters.monnies)
+
+print("name?")
+name = input()
+monsters.createMonster(name)
+monsters.listmons()
